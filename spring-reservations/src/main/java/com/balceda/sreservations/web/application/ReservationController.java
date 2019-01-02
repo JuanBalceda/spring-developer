348463233b40e13a7aa1d20043406686cff39a66
Juan Balceda <juanbalceda@gmail.com>
2019-01-03 00:27:07 +0100

    Refactoring DateFormating

--- spring-reservations/src/main/java/com/balceda/sreservations/web/application/ReservationController.java
+++ spring-reservations/src/main/java/com/balceda/sreservations/web/application/ReservationController.java
@@ -19,24 +19,13 @@ import java.util.List;
 @RequestMapping(value = "/reservations")
 public class ReservationController {
 
-    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
-
     @Autowired
     private ReservationService reservationService;
 
     @RequestMapping(method = RequestMethod.GET)
     public String getReservation(@RequestParam(value = "date", required = false) String dateString, Model model) {
-        Date date = null;
-        if (dateString != null){
-            try {
-                date = DATE_FORMAT.parse(dateString);
-            }catch (ParseException e){
-                date = new Date();
-            }
-        }else {
-            date = new Date();
-        }
-        List<RoomReservation> roomReservations = this.reservationService.getRoomReservationsForDate(date);
+
+        List<RoomReservation> roomReservations = this.reservationService.getRoomReservationsForDate(dateString);
         model.addAttribute("roomReservations", roomReservations);
         return "reservations";
     }
